565be1807c81cae516035d5e3a177ca725bba74b
Dmitrii <st_d.p.chernyshsev@ok654.ru>
2022-09-24 13:13:28 +0300

    quiz added

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+from colorama import Cursor
 from flask import Flask, render_template
 from random import randint
+import sqlite3 
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-    num = randint(1,6)
-    return render_template("index.html", num = num)
-
+    connection = sqlite3.connect("historyBot.sqlite3")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM tasks") 
+    result = cursor.fetchall()
+    print(result)
+    return render_template("index.html", result = result)
+app.run()
+    
+ 
+     
 
